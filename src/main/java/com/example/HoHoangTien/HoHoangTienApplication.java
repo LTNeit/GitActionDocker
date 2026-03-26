4812fa26397ec649b88c579eb479bf25c072f708
HoHoangTien <hohoangtien2412@gmail.com>
2026-03-26 10:36:36 +0700

    test

---
+++ src/main/java/com/example/HoHoangTien/HoHoangTienApplication.java
@@ -0,0 +1,13 @@
+package com.example.HoHoangTien;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoHoangTienApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoHoangTienApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HoHoangTien/HoHoangTienApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HoHoangTien;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoHoangTienApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
